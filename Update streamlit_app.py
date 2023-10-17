43b7005768cb2af2f2aeb780afce9a7b1e1040f0
devika-appdev3 <148254765+devika-appdev3@users.noreply.github.com>
2023-10-17 13:02:24 -0500

    Delete Update streamlit_app.py

--- Update streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit
-streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free_Range Egg')
