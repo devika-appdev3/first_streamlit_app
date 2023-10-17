c68f7705b2d0c730e56b1c35fc1219945287d7c5
devika-appdev3 <148254765+devika-appdev3@users.noreply.github.com>
2023-10-17 11:50:08 -0500

    Update and rename streamlit_app.py to Update streamlit_app.py

---
+++ Update streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit
+streamlit.title('My parents new healthy dinner')
+Breakfast Menu
+Omega 3 & Bluberry Oatmeal
+Kale, Spinach & Rocket Smoothie
+Hard-Boiled Free_Range Egg
--- streamlit_app.py
+++
@@ -1,2 +0,0 @@
-import streamlit
-streamlit.title('My parents new healthy dinner')
