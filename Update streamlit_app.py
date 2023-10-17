e7f16e15cce511bd3649f8e6e2ba2da2053efa8d
devika-appdev3 <148254765+devika-appdev3@users.noreply.github.com>
2023-10-17 12:35:50 -0500

    streamlit_app.py

--- Update streamlit_app.py
+++ Update streamlit_app.py
@@ -1,7 +1,3 @@
 import streamlit
-import random
 streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text(' Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free_Range Egg')
+
